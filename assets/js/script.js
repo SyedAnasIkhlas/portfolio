508c557c137d19aff254671eba393eaf182bd7f4
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2019-07-10 08:42:26 +0300

    improvment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-$('.owl-carousel').owlCarousel({
-    // loop:true,
-    margin:10,
-    nav:true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:false,
-    responsive:
-    {
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+$(document).ready(function() 
+{
+
+    // var url = window.location.href; // Returns full URL (https://example.com/path/example.html)
+
+    $('.owl-carousel').owlCarousel({
+        // loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        responsive:
+        {
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
         }
-    }
-})
+    })
+
+
+    $("#click").on('click', function() 
+    {
+        var y = $(window).scrollTop();
+        $('html, body').animate({ scrollTop: y + 260 })
+    });
+        
+    
+
+
+    
+});
+
+
+
